30ad38d1e2d26e06b9c33e3bf4d1d96df1539e1e
whittenburgsa <whittenburgsavannah@gmail.com>
2022-06-13 08:56:35 -0400

    initial game set up

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,35 @@
-// this creates a function named "fight"
-var playerName = window.prompt("What is your robot's name?");
-// What is this?
-console.log(playerName);
+var playerName = window.prompt("What is your name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
+//You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-console.log(10+10);
-// what is this?
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log("Our robot's name is" + playerName);
+var fight = function() {
+    //Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
 
-function fight() {
-    window.alert("The fight has begun!");
-  }
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");    
 
-  // fight();
+// put new code under this
+console.log(
+  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+
+//check enemy's health
+if (enemyHealth <= 0) {
+  window.alert(enemyName + "has died!");
+}
+
+else {
+  window.alert(enemyName + "still has" + enemyHealth + "health left.");
+}
+
+fight();
+
+};
+
